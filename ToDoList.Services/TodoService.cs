ac5992efd0bf1c84471989f6db1c3bd3b9642d38
wei.wei <monkalways@gmail.com>
2022-11-23 10:36:37 -0700

    Add one more todo

--- ToDoList.Services/TodoService.cs
+++ ToDoList.Services/TodoService.cs
@@ -9,7 +9,8 @@ namespace ToDoList.Services
             return new List<Todo>
             {
                 new Todo { Id = 1, Name = "Clean floor", IsComplete = false, DueDate = DateTime.Today },
-                new Todo { Id = 2, Name = "Walk the dog", IsComplete = false, DueDate = DateTime.Today.AddDays(1) }
+                new Todo { Id = 2, Name = "Walk the dog", IsComplete = false, DueDate = DateTime.Today.AddDays(1) },
+                new Todo { Id = 3, Name = "Walk the dog Again", IsComplete = false, DueDate = DateTime.Today.AddDays(1) }
             };
         }
     }
